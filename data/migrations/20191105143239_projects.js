3082b8cc3ad0ad298cc17b657a7158fc5b682e13
BuildTools <arthurpisakhov1@gmail.com>
2019-11-05 14:45:36 -0500

    setup migrations

---
+++ data/migrations/20191105143239_projects.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', table => {
+      table.increments();
+      table.string('name', 128).notNullable();
+      table.string('description', 256);
+      table.boolean('completed').defaultTo(false);
+      table.string('tasks');
+      table.string('resources');
+    })
+    .createTable('tasks', table => {
+      table.increments();
+      table.string('description', 256).notNullable();
+      table.string('notes', 256);
+      table.boolean('completed').defaultTo(false);
+      table.integer('project_id').unsigned().notNullable().references('projects.id')
+    })
+    .createTable('resources', table => {
+      table.increments();
+      table.string('name', 128).notNullable();
+      table.string('description', 256);
+      table.integer('project_id').unsigned().notNullable().references('projects.id')
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('recipes')
+};
