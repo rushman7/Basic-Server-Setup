41284640f08e6586ebb6f8d504549846cb2ac137
BuildTools <arthurpisakhov1@gmail.com>
2019-11-07 21:38:18 -0500

    added morgan and helmet

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const logger = require('./api/logger');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const projectRouter = require('./routes/projects-router');
 const taskRouter = require('./routes/tasks-router');
@@ -9,6 +11,8 @@ const resourceRouter = require('./routes/resources-router');
 const server = express();
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
+server.use(morgan("combined"));
+server.use(helmet());
 
 server.use('/api/projects', logger(), projectRouter);
 server.use('/api/tasks', logger(), taskRouter);
